cbaaf3d4e77bc9dfa13b7524eb63dad5957d968a
Qwertuhh <arihantjain132407+github@gmail.com>
2025-06-11 19:38:00 +0530

    feat: update version to 1.2.3 in package.json and leaky memory fix

--- src/index.ts
+++ src/index.ts
@@ -8,154 +8,130 @@ import copyFiles from "@/utils/copyFiles";
 import deleteFiles from "./utils/deleteFiles";
 
 /**
- * Copies files as specified in the configuration. If the configuration includes a
- * list of files to copy, each file is copied from the source to the destination
- * directories defined in the configuration. Logs the copying process to the console.
+ * Copies files as specified in the configuration.
  *
- * @param config - The configuration object containing copy instructions.
- * @returns A promise that resolves to `true` if files were copied, or `false` if no
- * files were specified for copying.
+ * @param config - The configuration object to use for resolving file paths.
+ * @returns Nothing.
+ * @throws Will log an error message if there is an issue copying a file.
  */
-
-const copyFilesFunction = (config: Config) =>
-  new Promise((resolve, reject) => {
-    if (config.copy) {
-      for (const copyConfig of config.copy) {
-        console.log("Coping files ...");
-        copyFiles(config.root, copyConfig);
-      }
-      console.log("Coping files completed");
-      resolve(true);
-    } else {
-      resolve(false);
+function copyFilesFunction(config: Config) {
+  if (config.copy) {
+    for (const copyConfig of config.copy) {
+      console.log("Coping files ...");
+      copyFiles(config.root, copyConfig);
     }
-  });
+    console.log("Coping files completed");
+  }
+}
 
 /**
- * Deletes files as specified in the configuration. If the configuration includes a
- * list of files to delete, each file is deleted from the specified path. Logs the
- * deletion process to the console.
+ * Deletes files as specified in the configuration.
  *
- * @param config - The configuration object containing delete instructions.
- * @returns A promise that resolves to `true` if files were deleted, or `false` if no
- * files were specified for deletion.
+ * @param config - The configuration object to use for resolving file paths.
+ * @returns Nothing.
+ * @throws Will log an error message if there is an issue deleting a file.
  */
-const deleteFilesFunction = (config: Config) =>
-  new Promise((resolve, reject) => {
-    if (config.delete) {
-      for (const filePath of config.delete) {
-        console.log("Deleting files ...");
-        deleteFiles(resolvePath(join(config.root, filePath)));
-        console.log("Deleting files completed");
-      }
-      resolve(true);
-    } else {
-      resolve(false);
+function deleteFilesFunction(config: Config) {
+  if (config.delete) {
+    for (const filePath of config.delete) {
+      console.log("Deleting files ...");
+      deleteFiles(resolvePath(join(config.root, filePath)));
+      console.log("Deleting files completed");
     }
-  });
+  }
+}
 
 /**
- * Updates import statements in specific files as specified in the configuration.
- * If the configuration includes a list of files, each file's import statements
- * are updated from the old import path to the new import path. If a file does not
- * exist, it logs an error and continues to the next file. Logs the result of the
- * import updates to the console.
+ * Applies priority import changes for specific files as specified in the configuration.
  *
- * @param config - The configuration object containing file-specific import change instructions.
- * @returns A promise that resolves to `true` when all specified files have been processed, or rejects with an error if an exception occurs.
+ * Iterates over each file configuration and changes the import paths from `toChangeFrom`
+ * to `toChange` for files that exist at the specified paths within the root directory.
+ * Logs messages indicating whether imports were changed or if no relevant imports were found.
+ * If a file does not exist at the specified path, logs an error message.
+ *
+ * @param config - The configuration object containing the file-specific import change details.
+ * @throws Will log an error message if a specified file does not exist.
  */
-const priorityFileChanges = (config: Config) =>
-  new Promise((resolve, reject) => {
-    try {
-      if (config.file) {
-        for (const fileConfig of config.file) {
-          let { path: filePath, toChange, toChangeFrom } = fileConfig;
-          filePath = resolvePath(join(config.root, filePath));
-          if (!existsSync(filePath)) {
-            console.error(`File not found: ${filePath}`);
-            continue;
-          }
-
-          console.log(
-            changeImportsOnce(filePath, toChange, toChangeFrom)
-              ? `Changed imports in ${filePath} from ${toChangeFrom} to ${toChange}`
-              : `No imports found that need updating in ${filePath}`
-          );
-        }
+function priorityFileChanges(config: Config) {
+  if (config.file) {
+    for (const fileConfig of config.file) {
+      let { path: filePath, toChange, toChangeFrom } = fileConfig;
+      filePath = resolvePath(join(config.root, filePath));
+      if (!existsSync(filePath)) {
+        console.error(`File not found: ${filePath}`);
+        continue;
       }
-      resolve(true);
-    } catch (error) {
-      reject(error);
+
+      console.log(
+        changeImportsOnce(filePath, toChange, toChangeFrom)
+          ? `Changed imports in ${filePath} from ${toChangeFrom} to ${toChange}`
+          : `No imports found that need updating in ${filePath}`
+      );
     }
-  });
+  }
+}
 
 /**
- * Changes import statements in all the files specified in the `filePaths` array as
- * specified in the configuration. For each file, all import statements are updated
- * from the old import path to the new import path as specified in the `changes`
- * array in the configuration. If a file does not exist, it logs an error and
- * continues to the next file. Logs the result of the import updates to the console.
+ * Iterates over each file path and changes the import paths from `toChangeFrom` to
+ * `toChange` for each file that exists at the specified paths within the root
+ * directory. Logs messages indicating whether imports were changed or if no relevant
+ * imports were found. If a file does not exist at the specified path, logs an error
+ * message.
  *
- * @param filePaths - An array of file paths to be processed.
- * @param config - The configuration object containing the import change instructions.
- * @returns A promise that resolves to `undefined` when all specified files have been
- * processed, or rejects with an error if an exception occurs.
+ * @param filePaths - An array of file paths to change imports in.
+ * @param config - The configuration object containing the import change details.
+ * @throws Will log an error message if a specified file does not exist.
  */
-const importsChanges = (filePaths: string[], config: Config) =>
-  new Promise<void>((resolve, reject) => {
-    console.log("Changing imports in files...");
-    try {
-      for (const filePath of filePaths) {
-        if (!existsSync(filePath)) {
-          console.error(`File not found: ${filePath}`);
-          continue;
-        }
-
-        // Change imports in each file based on the changes specified in the config
-        for (const change of config.changes) {
-          const { toChange, toChangeFrom } = change;
-          console.log(
-            changeImportsOnce(filePath, toChange, toChangeFrom)
-              ? `Changed imports in ${filePath} from ${toChangeFrom} to ${toChange}`
-              : `No imports found that need updating in ${filePath}`
-          );
-        }
-      }
-      console.log("Changing imports in files completed.");
-      resolve();
-    } catch (error) {
-      reject(error);
+function importsChanges(filePaths: string[], config: Config) {
+  console.log("Changing imports in files...");
+  for (const filePath of filePaths) {
+    if (!existsSync(filePath)) {
+      console.error(`File not found: ${filePath}`);
+      continue;
     }
-  });
 
+    // Change imports in each file based on the changes specified in the config
+    for (const change of config.changes) {
+      const { toChange, toChangeFrom } = change;
+      console.log(
+        changeImportsOnce(filePath, toChange, toChangeFrom)
+          ? `Changed imports in ${filePath} from ${toChangeFrom} to ${toChange}`
+          : `No imports found that need updating in ${filePath}`
+      );
+    }
+  }
+  console.log("Changing imports in files completed.");
+}
+
+
+/**
+ * Executes the post-imports processing based on the configuration specified 
+ * in the given JSON file, defaulting to "./package.json". This includes 
+ * copying and deleting specified files, applying priority import changes 
+ * to specific files, and modifying import paths in all matched files as per 
+ * the configuration.
+ *
+ * @param filePath - The path to the JSON configuration file. Defaults to "./package.json".
+ * @returns A boolean indicating the successful completion of the post-imports process.
+ * @throws Will log error messages for any file operation failures or configuration issues.
+ */
 function postImports(filePath: string = "./package.json"): boolean {
-  let result = false;
   const config: Config = getPostImportsConfig(filePath);
   //? To get all files for changing imports in the files specified in the config
   const filePaths = collectFilePaths(config);
 
-  const promises = [
-    //* To copy files
-    copyFilesFunction(config),
-    //* To delete files
-    deleteFilesFunction(config),
-    //* First change the file imports as specified in the config. to maintain the priority
-    priorityFileChanges(config),
-    //* Change imports in each file
-    importsChanges(filePaths, config),
-  ];
+  //* To copy files
+  copyFilesFunction(config),
+  //* To delete files
+  deleteFilesFunction(config),
+  //* First change the file imports as specified in the config. to maintain the priority
+  priorityFileChanges(config),
+  //* Change imports in each file
+  importsChanges(filePaths, config),
 
-  Promise.all(promises)
-    .then(() => {
-      result = true;
-      console.log("Post imports completed successfully.");
-    })
-    .catch(() => {
-      console.error("Post imports failed.");
-      process.exit(1);
-    });
+  console.log("Post imports completed successfully.");
 
-  return result;
+  return true;
 }
 
 export default postImports;
